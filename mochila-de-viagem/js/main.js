bb08f660338f9109aea947779644eebd637f5e64
diegoramiro <diegoramiro25@gmail.com>
2022-12-15 18:41:41 -0300

    novas funcoes, acoes e botao de excluir

--- mochila-de-viagem/js/main.js
+++ mochila-de-viagem/js/main.js
@@ -1,44 +1,73 @@
-const form = document.getElementById("novoItem");
-const lista = document.getElementById("lista");
-const itens = JSON.parse(localStorage.getItem("itens")) || [];
+const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+const itens = JSON.parse(localStorage.getItem("itens")) || []
 
 itens.forEach((elemento) => {
-	criaElemento(elemento);
-});
+  criaElemento(elemento)
+})
 
 form.addEventListener("submit", (evento) => {
-	evento.preventDefault();
+  evento.preventDefault()
 
-	const nome = evento.target.elements["nome"];
-	const quantidade = evento.target.elements["quantidade"];
+  const nome = evento.target.elements["nome"]
+  const quantidade = evento.target.elements["quantidade"]
 
-	const itemAtual = {
-		nome: nome.value,
-		quantidade: quantidade.value,
-	};
+  const existe = itens.find((elemento) => elemento.nome === nome.value)
 
-	criaElemento(itemAtual);
+  const itemAtual = {
+    nome: nome.value,
+    quantidade: quantidade.value,
+  }
 
-	itens.push(itemAtual);
+  if (existe) {
+    itemAtual.id = existe.id
 
-	localStorage.setItem("itens", JSON.stringify(itens));
+    atualizaElemento(itemAtual)
 
-	nome.value = "";
-	quantidade.value = "";
-});
+    itens[existe.id] = itemAtual
+  } else {
+    itemAtual.id = itens.length
+
+    criaElemento(itemAtual)
+
+    itens.push(itemAtual)
+  }
+
+  localStorage.setItem("itens", JSON.stringify(itens))
+
+  nome.value = ""
+  quantidade.value = ""
+})
 
 function criaElemento(item) {
-	const novoItem = document.createElement("li");
-	novoItem.classList.add("item");
+  const novoItem = document.createElement("li")
+  novoItem.classList.add("item")
 
-	const numeroItem = document.createElement("strong");
-	numeroItem.innerHTML = item.quantidade;
+  const numeroItem = document.createElement("strong")
+  numeroItem.innerHTML = item.quantidade
+  numeroItem.dataset.id = item.id
+  novoItem.appendChild(numeroItem)
+  novoItem.innerHTML += item.nome
 
-	novoItem.appendChild(numeroItem);
-	novoItem.innerHTML += item.nome;
+  novoItem.appendChild(botaoDeleta())
 
-	lista.appendChild(novoItem);
+  lista.appendChild(novoItem)
 }
-function recuperaItem() {
-	const itemSalvo = localStorage.getItem("itens");
+
+function atualizaElemento(item) {
+  document.querySelector("[data-id='" + item.id + "']").innerHTML =
+    item.quantidade
+}
+
+function botaoDeleta() {
+  const elementoBotao = document.createElement("button")
+  elementoBotao.innerText = "❌"
+
+  elementoBotao.addEventListener("click", function () {
+    deletaElemento(this.parentNode)
+  })
+  return elementoBotao
+}
+function deletaElemeto(tag) {
+  tag.remove()
 }
