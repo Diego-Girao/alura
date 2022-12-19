da0d68eaf768322df53cb46b10f3184f3ba28671
diegogirao <diegoramiro25@gmail.com>
2022-12-18 22:06:43 -0300

    feat: adicionado variaveis para busca cep e para mensagem de erro e passando para o html

--- alurabooks/script.js
+++ alurabooks/script.js
@@ -1,1 +1,28 @@
-console.log("Alurabooks");
+async function buscaEndereco(cep) {
+	const mensagemErro = document.getElementById("erro")
+	mensagemErro.innerHTML = ""
+	try {
+		const consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+		const consultaCepConvertido = await consultaCep.json()
+		if (consultaCepConvertido.erro) {
+			throw Error("CEP informado não existe !")
+		}
+		const cidade = document.getElementById("cidade")
+		const logradouro = document.getElementById("endereco")
+		const estado = document.getElementById("estado")
+		const bairro = document.getElementById("bairro")
+
+		cidade.value = consultaCepConvertido.localidade
+		logradouro.value = consultaCepConvertido.logradouro
+		estado.value = consultaCepConvertido.uf
+		bairro.value = consultaCepConvertido.bairro
+
+		console.log(consultaCepConvertido)
+		return consultaCepConvertido
+	} catch (erro) {
+		mensagemErro.innerHTML = `<p>CEP inválido, tente novamente !</p>`
+		console.log(erro)
+	}
+}
+const buscaCep = document.getElementById("cep")
+cep.addEventListener("focusout", () => buscaEndereco(cep.value))
