2569dcc8f1075f212e495d53ae5a8b3f814c350d
diegogirao <diegoramiro25@gmail.com>
2023-01-11 17:39:02 -0300

    criacao do arquivo de mostrar os videos e implementacao das funcoes

---
+++ aluraplay-requisicoes-main/js/mostrarVideos.js
@@ -0,0 +1,39 @@
+import { conectaApi } from "./conectaApi.js"
+
+const lista = document.querySelector("[data-lista]")
+
+export default function montarCard(titulo, descricao, url, imagem) {
+	const video = document.createElement("li")
+	video.className = "videos__item"
+	video.innerHTML = `
+  <iframe width="100%" height="72%" src="${url}"
+                title="${titulo}" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
+            <div class="descricao-video">
+                <img src="${imagem}" alt="logo canal alura">
+                <h3>${titulo}</h3>
+                <p>${descricao}</p>
+            </div>
+  `
+	return video
+}
+
+async function listaVideos() {
+	try {
+		const listaApi = await conectaApi.listaVideos()
+		listaApi.forEach((elemento) =>
+			lista.appendChild(
+				montarCard(
+					elemento.titulo,
+					elemento.descricao,
+					elemento.url,
+					elemento.imagem
+				)
+			)
+		)
+	} catch (error) {
+		lista.innerHTML = `<h2 class="mensagem__titulo">Não foi possível carregar a lista de vídeos 😞</h2>`
+	}
+}
+listaVideos()
