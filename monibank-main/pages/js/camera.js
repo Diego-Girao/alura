b0ade1045f031359734f1d4fbc1f5ef5b0a8f621
diegogirao <diegoramiro25@gmail.com>
2023-01-25 15:41:24 -0300

    implementacao da funcao para tirar a foto

--- monibank-main/pages/js/camera.js
+++ monibank-main/pages/js/camera.js
@@ -2,8 +2,10 @@ const btnIniciarCamera = document.querySelector("[data-video-botao]")
 const campoCamera = document.querySelector("[data-camera]")
 const video = document.querySelector("[data-video]")
 const btnTirarFoto = document.querySelector("[data-tirar-foto")
-const canva = document.querySelector("[data-video-canva]")
+const canva = document.querySelector("[data-video-canvas]")
 const mensagem = document.querySelector("[data-mensagem]")
+
+let imagemURL = ""
 
 btnIniciarCamera.addEventListener("click", async function () {
 	const iniciarVideo = await navigator.mediaDevices.getUserMedia({
@@ -14,4 +16,12 @@ btnIniciarCamera.addEventListener("click", async function () {
 	campoCamera.style.display = "block"
 
 	video.srcObject = iniciarVideo
+})
+
+btnTirarFoto.addEventListener("click", function () {
+	canva.getContext("2d").drawImage(video, 0, 0, canva.width, canva.height)
+	imagemURL = canva.toDataURL("image/jpeg")
+
+	campoCamera.style.display = "none"
+	mensagem.style.display = "block"
 })
