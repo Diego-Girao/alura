7a17221019259b113b76c15316e126207926a612
diegogirao <diegoramiro25@gmail.com>
2023-01-25 15:18:55 -0300

    criacao das variaveis de tirar a foto

--- monibank-main/pages/js/camera.js
+++ monibank-main/pages/js/camera.js
@@ -1,6 +1,9 @@
 const btnIniciarCamera = document.querySelector("[data-video-botao]")
 const campoCamera = document.querySelector("[data-camera]")
 const video = document.querySelector("[data-video]")
+const btnTirarFoto = document.querySelector("[data-tirar-foto")
+const canva = document.querySelector("[data-video-canva]")
+const mensagem = document.querySelector("[data-mensagem]")
 
 btnIniciarCamera.addEventListener("click", async function () {
 	const iniciarVideo = await navigator.mediaDevices.getUserMedia({
